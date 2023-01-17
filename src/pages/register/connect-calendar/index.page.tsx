678fed06fe9f063485bc2b1220b67862a23a231d
henriqueholz <henriqueholz@live.com>
2023-01-16 22:22:01 -0300

    Adding error message and adjusts

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,10 +1,21 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { signIn } from 'next-auth/react'
-import { ArrowRight } from 'phosphor-react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { ArrowRight, Check } from 'phosphor-react'
 import { Container, Header } from '../styles'
-import { ConnectBox, ConnectItem } from './styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
 
 export default function ConnectCalendar() {
+  const session = useSession()
+  const router = useRouter()
+
+  const hasAuthError = !!router.query.error
+  const isSignedIn = session.status === 'authenticated'
+
+  async function handleConnectCalendar() {
+    await signIn('google')
+  }
+
   return (
     <>
       <div title="Conecte sua agenda do Google | Ignite Call" />
@@ -23,17 +34,30 @@ export default function ConnectCalendar() {
         <ConnectBox>
           <ConnectItem>
             <Text>Google Calendar</Text>
-            <Button
-              variant="secondary"
-              size="sm"
-              onClick={() => signIn('google')}
-            >
-              Conectar
-              <ArrowRight />
-            </Button>
+            {isSignedIn ? (
+              <Button size="sm">
+                Conectado
+                <Check />
+              </Button>
+            ) : (
+              <Button
+                variant="secondary"
+                size="sm"
+                onClick={handleConnectCalendar}
+              >
+                Conectar
+                <ArrowRight />
+              </Button>
+            )}
           </ConnectItem>
 
-          <Button type="submit">
+          {hasAuthError && (
+            <AuthError size="sm">
+              Falha ao se conectar ao google, verifique permissões de acesso
+            </AuthError>
+          )}
+
+          <Button type="submit" disabled={!isSignedIn}>
             Próximo passo
             <ArrowRight />
           </Button>
